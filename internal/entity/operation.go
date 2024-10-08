2efb0f0de82010c1f86494d542988e99c1a0c50f
oleska <oleska1601@gmail.com>
2024-10-08 21:36:07 +0300

    add db, change entities

--- internal/entity/operation.go
+++ internal/entity/operation.go
@@ -2,7 +2,7 @@ package entity
 
 type Operation struct {
 	ID        int    `json:"id"`
-	AccountID int    `json:"account-id"`
+	AccountID int    `json:"account_id"`
 	Amount    int    `json:"amount"`
 	Type      string `json:"type"` //topup or deduct
 	Comment   string `json:"comment"`
